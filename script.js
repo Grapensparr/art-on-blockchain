dc2f23a338087c380fedd6b13a193d1b3a317714
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2022-12-13 13:13:26 +0100

    Pseudo code

---
+++ script.js
@@ -0,0 +1,31 @@
+//Klass för användare
+    //Array med konstverk som användaren äger
+    //Kan skicka konstverket
+    //Kan ta emot konstverk
+    //Kan registrera konstverk
+    //Kan få ut lista på vilka konstverket äger
+
+//Användarlista
+
+//Producera block
+
+//Valideringsfunktion
+    //Registreringar av konstverk på användare
+
+//Klass för konstverk
+    //ID
+    //Vem som äger verket
+    //Titel
+    //Pris
+    //Ägare
+    //Tillverkningsår
+
+//Inloggningsfunktion/Utloggning
+
+//Skapa användare
+
+//Registrera konstverk
+
+//API
+//https://www.programmableweb.com/api/metropolitan-museum-art-met-collection-rest-api-v10
+
